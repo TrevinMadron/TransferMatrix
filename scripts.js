65a4344d70bc90488e0c959079571415871a7ab1
Trevin Madron <115761468+TrevinMadron@users.noreply.github.com>
2023-04-02 15:48:03 -0500

    Now pulls data directly from the .unl files

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 // Sets the view variables and sets the SchoolView to be visible on page load and hides ClassView
 const schoolView = document.getElementById('selectschool');
 const classView = document.getElementById('selectclass');
@@ -18,58 +17,30 @@ let transferWorkList = document.getElementById('transferworktable');
 
 ----------- */
 
+
 const schoolSearchBox = document.getElementById('schoolsearch')
-let schoolList = [
-    {
-        "number" : "1",
-        "name" : "ABILENE WY CHRISTIAN UNIV",
-        "state" : "WY"
-    },
-    {
-        "number" : "2",
-        "name" : "ACADEMY OF ART UNIV CA",
-        "state" : "CA"
-    },
-    {
-        "number" : "3",
-        "name" : "ACT EDUC SOLUTIONS NO SYDNEY AS",
-        "state" : "ZZ"
-    },
-    {
-        "number" : "4",
-        "name" : "ADAMS STATE UNIV ALAMOSA CO",
-        "state" : "CO"
-    },
-    {
-        "number" : "5",
-        "name" : "ADELPHI UNIV GARDEN CITY NY",
-        "state" : "NY"
-    },
-    {
-        "number" : "6",
-        "name" : "TEST SCHOOL THINGY",
-        "state" : "OK"
-    },
-]
+let schoolList = await fetchUnl("names.unl");
+populateSchools(schoolList);
 
 // RUNS ON PAGE LOAD - Adds all schools to list and adds event listener to each element for click detection
-$("#schooltable").empty();
-for(let i in schoolList) {
-    $("#schooltable").append(
-        "<li><button id='" + schoolList[i].number + "'>" +
-        schoolList[i].name + " - (" + schoolList[i].state + ")" +
-        "</button></li>"   
-    );
-    document.getElementById(schoolList[i].number).addEventListener('click', () => {
-        selectSchool(schoolList[i].number, schoolList[i].name);
+function populateSchools(schoolList) {
+    $("#schooltable").empty();
+    for(let i in schoolList) {
+        $("#schooltable").append(
+            "<li><button id='" + schoolList[i].number + "'>" +
+            schoolList[i].name + " - (" + schoolList[i].state + ")" +
+            "</button></li>"   
+        );
+        document.getElementById(schoolList[i].number).addEventListener('click', () => {
+            selectSchool(schoolList[i].number, schoolList[i].name);
         });
+    }
 }
 
 // Monitors for changes in the school search box and updates school list
 const schoolInputHandler = function(e) {
     $("#schooltable").empty();
     let search = e.target.value;
-
     for(let i in schoolList){
         if(schoolList[i].name.toLowerCase().includes(search.toLowerCase()) || schoolList[i].state.toLowerCase().includes(search.toLowerCase())){
             $("#schooltable").append(
@@ -79,23 +50,19 @@ const schoolInputHandler = function(e) {
             );
             document.getElementById(schoolList[i].number).addEventListener('click', () => {
                 selectSchool(schoolList[i].number, schoolList[i].name);
-                });
+            });
         }
     }
-    }
+}
 schoolSearchBox.addEventListener('input', schoolInputHandler);
-
 
 // Loads class list based on selected (clicked) school and appends classes to list
 function selectSchool(schoolNumber, schoolName) {
     // Sets global current school variable to currently selected school
     currentSchoolNumber = schoolNumber;
-
     document.getElementById('selectschool').style.display = 'none';
     document.getElementById('selectclass').style.display = 'block';
-
     document.getElementById('schoolname').textContent = schoolName;
-
     for(let i in classList) {
         if(classList[i].number == currentSchoolNumber) {
             if(classList[i].number == schoolNumber) {
@@ -126,12 +93,12 @@ const navBackButton = document.getElementById("navbackbutton")
 navBackButton.addEventListener('click', () => {
     currentSchoolNumber = 0;
     $("#classtable").empty();
-
     document.getElementById('selectschool').style.display = 'block';
     document.getElementById('selectclass').style.display = 'none';
 });
 
-let classList = [
+let classList = await fetchUnl("courses.unl");
+/*let classList = [
     {
         "number" : "4",
         "code" : "AR 103",
@@ -159,7 +126,7 @@ let classList = [
         "type" : "UNI",
         "classification" : "LOWER"
     }
-]
+]*/
 
 // Monitors for changes in the class search box and updates class list
 const classInputHandler = function(e) {
@@ -171,7 +138,8 @@ const classInputHandler = function(e) {
             if(classList[i].name.toLowerCase().includes(search.toLowerCase()) || classList[i].code.toLowerCase().includes(search.toLowerCase())){
                 $("#classtable").append(
                     "<li><div><div class='classInfo'>" +
-                    classList[i].name + " - (" + classList[i].code + ")</div><button id='classAdd'>+ Add</button></div></li>"    
+                    classList[i].name + " - (" + classList[i].code + ")</div><button class='classAdd' id='" +
+                    classList[i].code + "'>+ Add</button></div></li>" 
                 );
 
                 document.getElementById(classList[i].code).addEventListener('click', () => {
@@ -180,7 +148,7 @@ const classInputHandler = function(e) {
             }
         }
     }
-    }
+}
 classSearchBox.addEventListener('input', classInputHandler);
 
 function AddClassToList(course) {
@@ -188,8 +156,11 @@ function AddClassToList(course) {
     RefreshList();
 }
 
+// WIP -- Will remove any courses with the same course number regardless of school
 function RemoveClassFromList(number, course) {
-    savedClassList = savedClassList.filter(function(e) {return e.number != number && e.course != course});
+    savedClassList = savedClassList.filter(function(e) {
+        return e.code != course;
+    });
     RefreshList();
 }
 
@@ -200,55 +171,58 @@ function RefreshList() {
             "<li><div class='transferitem'><div style='float:left;width:40%'>" +
             savedClassList[i].name + " - (" + savedClassList[i].code + ")</div><div style='float:left;width:5%;'>→</div><div style='float:left;width:40%;'>" +
             savedClassList[i].equiv + " - (" + savedClassList[i].classification +
-            "-LEVEL)</div><div style='float:left;width:15%;'><button class='matrixRemoveButton' id='matrixRemove" + savedClassList[i].code + "'>- Remove</button></div></div></li>"    
+            "-LEVEL)</div><div style='float:left;width:15%;'><button class='matrixRemoveButton' id='matrixRemove" + savedClassList[i].number + savedClassList[i].code + "'>- Remove</button></div></div></li>"    
         );
 
-        document.getElementById('matrixRemove' + savedClassList[i].code).addEventListener('click', function(e) {
-            RemoveClassFromList(this.parentNode.number, this.parentNode.course)
-            
+        document.getElementById('matrixRemove' + savedClassList[i].number + savedClassList[i].code).addEventListener('click', function(e) {
+            RemoveClassFromList(savedClassList[i].number, savedClassList[i].code)
         });
     }
 }
 
 
-/*var txtFile = new XMLHttpRequest();
-txtFile.open("GET", "https://ecutransfermatrix.s3.us-east-2.amazonaws.com/names.uml", true);
-txtFile.onreadystatechange = function() {
-    alert(txtFile.readyState);
-  if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-    if (txtFile.status === 200) {  // Makes sure it's found the file.
-      allText = txtFile.responseText;
-      lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-      alert(allText);
+
+/*  ----------
+
+# REGION - Unl Parsing
+# All code and functionality for fetching and handling of data files
+
+----------- */
+
+// Fetches the specified UNL file and loads it into an object format
+async function fetchUnl(file) {
+    let response = await fetch("/" + file);
+
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+        let data = await response.text();
+        let delimiter = ";"
+        // handle data
+        let structure = "number;name;state;\n";
+        if (file == "courses.unl") {
+            structure = "number;code;name;equiv;date;type;classification;\n";
+        }
+        const titles = structure.slice(0, structure.indexOf('\n')).split(delimiter);
+        
+        return data
+            // Returns each line as string (Not sure if needed..?)
+            //.slice(data.indexOf('\n'))
+        
+            // Splits each line into substrings 
+            .split('\n').map(v => {
+            const values = v.split(delimiter);
+            return titles.reduce(
+                (obj, title, index) => ((obj[title] = values[index]), obj),
+                {}
+            );
+        });
     }
-  }
 }
 
-/* Will be (somehow) used to incorporate the data files into the site
-function getSchoolList() {
-    let delimiter = ";"
-    const reader = new FileReader()
-    reader.readAsText()
 
-    reader.onload = function (e) {
-        const text = e.target.result;
-        const data = umlToArray(text);
-        };
-
-    const rows = str.slice(str.indexOf("\n")).split("\n");
-
-    const arr = rows.map(function (row) {
-        const values = row.split(delimiter);
-        const el = headers.reduce(function (object, header, index) {
-            object[header] = values[index];
-            return object;
-        }, {});
-        return el;
-    });
-    return arr;
-}
-
-const client = new S3Client({})
+/*const client = new S3Client({})
 
 export const main = async () => {
   const command = new GetObjectCommand({
